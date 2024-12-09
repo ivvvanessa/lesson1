ab36fab3f65b41e97400f0df52d90bf3791779e3
ivvvanessa <ivvvanessa@icloud.com>
2024-12-10 00:09:57 +0300

    Update 1 lesson.py

--- 1 lesson.py
+++ 1 lesson.py
@@ -1,11 +1,13 @@
 #1st program
-print((9*0.5)*5)
+print((9**0.5)*5)
 #2nd program
-print(9.99 > 9.98)
-print(1000==1000.1)
+print(9.99>9.98 and 1000 !=1000.1)
 #3rd program
 print(2*2+2) #без приоритета
-print(2*(2+2)) # с приоритетом
+print(2*(2+2)) #с приоритетом
 print((2*2+2)==(2*(2+2)))
-#4th program
-print(int(123.456*10))
+#4th progam
+num = float('123.456')
+num*=10
+int(num)% 10
+print(int(num)% 10)
